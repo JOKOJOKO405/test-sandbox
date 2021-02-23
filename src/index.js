bcaf282007680e037ddb0c36eee6c32c761e6678
JOKOJOKO405 <56413237+JOKOJOKO405@users.noreply.github.com>
2021-02-23 15:32:22 +0900

    TODO追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const text = document.getElementById("addtext").value;
+  document.getElementById("addtext").value = "";
+
+  const div = document.createElement("div");
+  div.className = "list-low";
+
+  const li = document.createElement("li");
+  li.innerText = text;
+
+  // 完了ボタン
+  const donebtn = document.createElement("button");
+  donebtn.innerText = "done";
+  donebtn.addEventListener("click", () => {
+    console.log("done");
+  });
+
+  // 削除ボタン
+  const delbtn = document.createElement("button");
+  delbtn.innerText = "delete";
+  delbtn.addEventListener("click", () => {
+    console.log("del");
+  });
+
+  div.appendChild(li);
+  div.appendChild(donebtn);
+  div.appendChild(delbtn);
+
+  document.getElementById("incomp-list").appendChild(div);
+};
+
+document
+  .getElementById("add-btn")
+  .addEventListener("click", () => onClickAdd());
